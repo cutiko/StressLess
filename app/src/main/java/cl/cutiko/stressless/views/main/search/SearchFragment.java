f466f1bc6fd305d6b2674aadfe911993a92c7a1c
cutiko <cutiko@gmail.com>
2016-07-27 13:46:58 -0400

    WARNING: lower target sdk version. WIP: search bar

--- app/src/main/java/cl/cutiko/stressless/views/main/search/SearchFragment.java
+++ app/src/main/java/cl/cutiko/stressless/views/main/search/SearchFragment.java
@@ -1,12 +1,15 @@
 package cl.cutiko.stressless.views.main.search;
 
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 
 import cl.cutiko.stressless.R;
 
@@ -26,7 +29,27 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         View main = inflater.inflate(R.layout.fragment_search, container, false);
 
-        AutoCompleteTextView autoComplete = (AutoCompleteTextView) main.findViewById(R.id.searchBar);
+        ImageView expander = (ImageView) main.findViewById(R.id.searchIv);
+
+        final AutoCompleteTextView autoComplete = (AutoCompleteTextView) main.findViewById(R.id.searchBar);
+        autoComplete.setVisibility(View.GONE);
+
+        Display display = getActivity().getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        final int width = size.x;
+        autoComplete.animate().translationX(width-100).start();
+
+        expander.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (autoComplete.getVisibility() == View.GONE) {
+                    autoComplete.setVisibility(View.VISIBLE);
+                    autoComplete.animate().translationX(-width).setDuration(400).start();
+                }
+            }
+        });
+
 
         return main;
     }
