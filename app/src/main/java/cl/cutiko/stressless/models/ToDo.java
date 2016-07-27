3afe103ee726db8885acac0cc72538f0fc666409
cutiko <cutiko@gmail.com>
2016-07-27 11:49:50 -0400

    ADD: more variables

--- app/src/main/java/cl/cutiko/stressless/models/ToDo.java
+++ app/src/main/java/cl/cutiko/stressless/models/ToDo.java
@@ -5,17 +5,19 @@ import com.orm.SugarRecord;
 /**
  * Created by cutiko on 27-07-16.
  */
-public class ToDo extends SugarRecord {
+public class Todo extends SugarRecord {
 
-    private String name;
-    private boolean done;
+    private String name, description;
+    private boolean done, archived;
 
-    public ToDo() {
+    public Todo() {
     }
 
-    public ToDo(String name) {
+    public Todo(String name) {
         this.name = name;
-        this.done = false;
+        done = false;
+        archived = false;
+        description = null;
     }
 
     public String getName() {
@@ -26,11 +28,27 @@ public class ToDo extends SugarRecord {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public boolean isDone() {
         return done;
     }
 
     public void setDone(boolean done) {
         this.done = done;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
     }
 }
