864707053007e6cc30e79f842d14f2d748cf984f
cutiko <cutiko@gmail.com>
2016-07-28 08:35:23 -0400

    ADD: save for description

--- app/src/main/java/cl/cutiko/stressless/views/details/DetailsActivity.java
+++ app/src/main/java/cl/cutiko/stressless/views/details/DetailsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import cl.cutiko.stressless.R;
 import cl.cutiko.stressless.models.Todo;
@@ -12,6 +13,7 @@ import cl.cutiko.stressless.views.main.todoList.TodoListFragment;
 public class DetailsActivity extends AppCompatActivity {
 
     private Todo todo;
+    private EditText descriptionEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,14 @@ public class DetailsActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+
+        descriptionEt = (EditText) findViewById(R.id.descriptionEt);
+        setDescription();
+    }
+
+    private void setDescription(){
+        String description = (todo.getDescription() != null) ? todo.getDescription() : "";
+        descriptionEt.setText(description);
     }
 
     @Override
@@ -34,7 +44,21 @@ public class DetailsActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        saveDescription();
+    }
+
+    @Override
     public void onBackPressed() {
         finish();
+    }
+
+    private void saveDescription(){
+        String description  = descriptionEt.getText().toString();
+        if (description != null && !description.isEmpty() && !description.equals("") && description.trim().length() > 0) {
+            todo.setDescription(description);
+            todo.save();
+        }
     }
 }
