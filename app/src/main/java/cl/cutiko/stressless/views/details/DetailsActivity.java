65e1a17e01505ef044dd1578449f6c5fd809e939
cutiko <cutiko@gmail.com>
2016-07-28 08:15:32 -0400

    ADD: title for details activity

--- app/src/main/java/cl/cutiko/stressless/views/details/DetailsActivity.java
+++ app/src/main/java/cl/cutiko/stressless/views/details/DetailsActivity.java
@@ -1,15 +1,40 @@
 package cl.cutiko.stressless.views.details;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import cl.cutiko.stressless.R;
+import cl.cutiko.stressless.models.Todo;
+import cl.cutiko.stressless.views.main.todoList.TodoListFragment;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private Todo todo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        todo = Todo.findById(Todo.class, getIntent().getLongExtra(TodoListFragment.TODO_ID, 0));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(todo.getName());
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDefaultDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
